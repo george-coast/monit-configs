04053dc18ce1643848b583edc388534d6b2d4e8c
george-coast <george.taylor@coastautonomous.com>
2025-06-12 16:49:09 -0400

    Delete services/notify_admin.sh

--- services/notify_admin.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Simple notification script example
-echo "$(date): Service $1 has changed state to $2" >> /var/log/monit/notify.log
-# Here you could add email or messaging integration
